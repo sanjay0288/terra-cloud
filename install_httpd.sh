fe40db52fad4130aaff5e5762ae0e86593bf3fd8
gshashi1408 <154942929+gshashi1408@users.noreply.github.com>
2024-03-04 15:14:20 +0530

    Update install_httpd.sh

--- install_httpd.sh
+++ install_httpd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
+sudo apt update -y
+sudo apt install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
 echo "Hello World from $(hostname -f)" > /var/www/html/index.html
