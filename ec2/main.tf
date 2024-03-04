bfa4d84b70dc4514b96b7e4ff30869b8b467efcb
gshashi1408 <154942929+gshashi1408@users.noreply.github.com>
2024-03-04 16:18:25 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,11 +1,18 @@
 #security group
-resource "aws_security_group" "webserver23_access" {
-        name = "webserver23_access"
+resource "aws_security_group" "webserver232_access" {
+        name = "webserver232_access"
         description = "allow ssh and http"
 
         ingress {
                 from_port = 80
                 to_port = 80
+                protocol = "tcp"
+                cidr_blocks = ["0.0.0.0/0"]
+        }
+
+        ingress {
+                from_port = 90
+                to_port = 90
                 protocol = "tcp"
                 cidr_blocks = ["0.0.0.0/0"]
         }
@@ -30,12 +37,12 @@ resource "aws_security_group" "webserver23_access" {
 resource "aws_instance" "ourfirst" {
   ami           = "ami-03bb6d83c60fc5f7c"
   availability_zone = "ap-south-1a"
-  instance_type = "t2.micro"
-  security_groups = ["${aws_security_group.webserver23_access.name}"]
+  instance_type = "t2.medium"
+  security_groups = ["${aws_security_group.webserver232_access.name}"]
   key_name = "key2"
   user_data = filebase64("install_httpd.sh")
   tags = {
-    Name  = "ec2-test"
+    Name  = "ec2-test12"
     Location = "Mumbai"
   }
 
